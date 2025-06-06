64906e4caed4f196e4c34931132016ed122ac955
mihnikola <mihailovic93.nikola@gmail.com>
2025-06-06 09:55:02 +0200

    not duplicate any more

--- app/components/login/hooks/useEmail.js
+++ app/components/login/hooks/useEmail.js
@@ -3,13 +3,21 @@ import { useState, useCallback } from "react";
 
 const useEmail = () => {
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
 
   const handleEmailChange = useCallback((text) => {
     const trimmedEmail = text.trim().toLowerCase();
+    const hasInternalSpaces = text.includes(" ") || trimmedEmail.includes(" "); // Simple check for any space
+
     setEmail(trimmedEmail);
+    if (hasInternalSpaces) {
+      setEmailError("Please enter a valid email address.");
+    } else {
+      setEmailError(""); // No errors
+    }
   }, []);
 
-  return { email, handleEmailChange, setEmail };
+  return { email, emailError, handleEmailChange, setEmail };
 };
 
 export default useEmail;
