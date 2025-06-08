746fb28e203b2578dba32776fe3dd18c6ad0fb28
mihnikola <mihailovic93.nikola@gmail.com>
2025-06-08 13:51:13 +0200

    sdas

--- app/components/login/hooks/useEmail.js
+++ app/components/login/hooks/useEmail.js
@@ -7,7 +7,7 @@ const useEmail = () => {
 
   const handleEmailChange = useCallback((text) => {
     const trimmedEmail = text.trim().toLowerCase();
-    if (trimmedEmail.includes(" ") || !emailRegex.test(trimmedEmail)) {
+    if (trimmedEmail) {
       setEmailError("Please enter a valid email address.");
     } else {
       setEmailError(""); // No errors
