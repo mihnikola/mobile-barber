1e50f4bc4191e2ee2cd20e0145f02f8f322a6e14
mihnikola <mihailovic93.nikola@gmail.com>
2025-07-22 23:15:27 +0200

    try gmaps

--- app/components/location/hooks/useOpenGoogleMaps.tsx
+++ app/components/location/hooks/useOpenGoogleMaps.tsx
@@ -1,68 +1,24 @@
-import { Alert, Linking, Platform } from "react-native";
-import * as Location from "expo-location";
+import { useCallback } from 'react';
+import { Linking, Platform } from 'react-native';
 
-const useOpenGoogleMaps = () => {
-  const openGoogleMapsRoute = async (
-    destinationLatitude,
-    destinationLongitude,
-    destinationName = "Destination"
-  ) => {
-    try {
-      // 1. Request foreground location permission
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        Alert.alert(
-          "Location Permission Denied",
-          "Please grant location permission to get your current location and show the route."
-        );
-        return;
-      }
+/**
+ * Custom hook to open Google Maps with either location or directions.
+ */
+export const useOpenGoogleMaps = () => {
+  const openLocation = useCallback((latitude, longitude) => {
+    const url = Platform.select({
+      ios: `http://maps.apple.com/?ll=${latitude},${longitude}`,
+      android: `geo:${latitude},${longitude}?q=${latitude},${longitude}`
+    });
 
-      // 2. Get current location
-      let location = await Location.getCurrentPositionAsync({});
-      const { latitude, longitude } = location.coords;
+    Linking.openURL(url).catch(err => console.error('Failed to open map:', err));
+  }, []);
 
-      // 3. Construct Google Maps URL
-      const origin = `${latitude},${longitude}`;
-      const destination = `${destinationLatitude},${destinationLongitude}`;
-      const travelMode = "driving"; // Options: 'driving', 'walking', 'bicycling', 'transit'
+  const openGoogleMapsRoute = useCallback((destinationLat, destinationLng) => {
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${destinationLat},${destinationLng}`;
 
-      let url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=${travelMode}`;
+    Linking.openURL(url).catch(err => console.error('Failed to open directions:', err));
+  }, []);
 
-      // For iOS, try the Google Maps app scheme first, then fall back to Apple Maps
-      if (Platform.OS === "ios") {
-        const googleMapsUrl = `comgooglemaps://?saddr=${origin}&daddr=${destination}&directionsmode=${travelMode}`;
-        const appleMapsUrl = `http://maps.apple.com/?saddr=${origin}&daddr=${destination}`;
-
-        const canOpenGoogleMapsApp = await Linking.canOpenURL(googleMapsUrl);
-
-        if (canOpenGoogleMapsApp) {
-          url = googleMapsUrl;
-        } else {
-          url = appleMapsUrl;
-        }
-      }
-
-      // 4. Check if the URL can be opened and then open it
-      const supported = await Linking.canOpenURL(url);
-
-      if (supported) {
-        await Linking.openURL(url);
-      } else {
-        Alert.alert(
-          "Cannot Open Maps",
-          `It seems your device cannot open maps using the URL: ${url}. Please ensure you have a maps app installed.`
-        );
-      }
-    } catch (error) {
-      console.error("Error opening maps:", error);
-      Alert.alert(
-        "Error",
-        "An error occurred while trying to open maps. Please ensure location services are enabled and try again later."
-      );
-    }
-  };
-  return { openGoogleMapsRoute };
+  return { openLocation, openGoogleMapsRoute };
 };
-
-export default useOpenGoogleMaps;
