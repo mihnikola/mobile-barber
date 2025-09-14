0999b080ea72a3b6a5c35f65ea289759354f2fc4
mihnikola <mihailovic93.nikola@gmail.com>
2025-09-15 00:38:29 +0200

    cech

--- components/infoapp/privacypolicy.js
+++ components/infoapp/privacypolicy.js
@@ -5,36 +5,19 @@ import {
   Image,
   StyleSheet,
   ScrollView,
-  TouchableOpacity,
-  Linking,
   StatusBar,
 } from "react-native";
 
 const privacypolicy = () => {
-  const supportEmail = "support@fta.com";
-  const privacyPolicyURL = "[Link to your Privacy Policy]"; // IMPORTANT!
-  const termsOfServiceURL = "[Link to your Terms of Service]"; // IMPORTANT!
+
   const developerName = "FusionTech Agency";
 
-  const {localization} = useLocalization();
+  const { localization } = useLocalization();
 
-  const openLink = async (url) => {
-    try {
-      const supported = await Linking.canOpenURL(url);
-      if (supported) {
-        await Linking.openURL(url);
-      } else {
-        alert(`Don't know how to open this URL: ${url}`);
-      }
-    } catch (error) {
-      console.error("An error occurred while opening the link:", error);
-      alert("Could not open the link.");
-    }
-  };
+
   return (
     <ScrollView style={styles.container}>
       <StatusBar backgroundColor="black" barStyle="dark-content" />
-
       <Image
         source={require("@/assets/images/settingsImage.jpg")}
         style={styles.headerImage}
@@ -44,27 +27,12 @@ const privacypolicy = () => {
       </View>
       <View style={styles.sectionContainer}>
         <Text style={styles.text}>{localization.SETTINGS.LEGAL.update}</Text>
-        <Text style={styles.paragraph}>{localization.SETTINGS.LEGAL.paragraph}</Text>
-        <View style={styles.section}>
-          <Text style={styles.label}>{localization.SETTINGS.LEGAL.support}</Text>
-          <TouchableOpacity
-            onPress={() => Linking.openURL(`mailto:${supportEmail}`)}
-          >
-            <Text style={styles.linkText}>{supportEmail}</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.legalSection}>
-          <TouchableOpacity onPress={() => openLink(privacyPolicyURL)}>
-            <Text style={styles.linkText}>{localization.SETTINGS.LEGAL.footerTitle}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => openLink(termsOfServiceURL)}>
-            <Text style={styles.linkText}>{localization.SETTINGS.LEGAL.footerTerms}</Text>
-          </TouchableOpacity>
-        </View>
-
+        <Text style={styles.paragraph}>
+          {localization.SETTINGS.LEGAL.paragraph}
+        
+        </Text>
         <Text style={styles.copyright}>
-         {localization.SETTINGS.LEGAL.footerBottom} {developerName}
+          {localization.SETTINGS.LEGAL.footerBottom} {developerName}
         </Text>
       </View>
     </ScrollView>
@@ -72,15 +40,10 @@ const privacypolicy = () => {
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: "black",
   },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    color: "white",
-  },
+
   sectionContainer: {
     padding: 20,
   },
@@ -104,57 +67,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 24,
   },
-  linkText: {
-    fontSize: 16,
-    color: "#007bff",
-    textDecorationLine: "underline",
-    marginBottom: 5,
-  },
-  section: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "white",
-    marginBottom: 5,
-  },
-  legalSection: {
-    marginTop: 10,
-    marginBottom: 10,
-    alignItems: "center",
-  },
+
   paragraph: {
     fontSize: 16,
     lineHeight: 24,
     color: "white",
     textAlign: "justify",
   },
-  headerImage: {
-    width: "100%",
-    height: 180,
-    opacity: 0.2,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "white",
-    marginBottom: 30,
-    textAlign: "center",
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "white",
-    marginBottom: 5,
-  },
-  value: {
-    fontSize: 16,
-    color: "white",
-    lineHeight: 24,
-  },
   copyright: {
+    marginBottom:50,
     fontSize: 14,
     color: "white",
     textAlign: "center",
