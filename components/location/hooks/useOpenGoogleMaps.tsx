3a96c82a7c0b0df506362a96d40e3dd1f2d18f77
mihnikola <mihailovic93.nikola@gmail.com>
2025-09-18 18:24:59 +0200

    don

--- components/location/hooks/useOpenGoogleMaps.tsx
+++ components/location/hooks/useOpenGoogleMaps.tsx
@@ -1,19 +1,19 @@
 import { Alert, Linking } from "react-native";
 
 export const useOpenGoogleMaps = () => {
- const openGoogleMapsRoute = async (url) => {
-  try {
-    const supported = await Linking.canOpenURL(url);
-    console.log("url",url)
-    if (supported) {
-      await Linking.openURL(url);
-    } else {
-      Alert.alert("Error", "Can't open this URL on your device");
+  const openGoogleMapsRoute = async (url) => {
+    try {
+      const supported = await Linking.canOpenURL(url);
+      console.log("url", url);
+      if (supported) {
+        await Linking.openURL(url);
+      } else {
+        Alert.alert("Error", "Can't open this URL on your device");
+      }
+    } catch (error) {
+      Alert.alert("Error", "Failed to open URL");
     }
-  } catch (error) {
-    Alert.alert("Error", "Failed to open URL");
-  }
-};
+  };
 
   return { openGoogleMapsRoute };
 };
