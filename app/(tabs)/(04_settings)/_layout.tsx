5d848de631d626cdd3f2df62358dc31d199fe010
mihnikola <mihailovic93.nikola@gmail.com>
2026-01-05 19:17:10 +0100

    ovo radi to su one dve stavke sto pise uradjeno

--- app/(tabs)/(04_settings)/_layout.tsx
+++ app/(tabs)/(04_settings)/_layout.tsx
@@ -1,6 +1,4 @@
-import { Ionicons } from "@expo/vector-icons";
-import { router, Stack } from "expo-router";
-import { TouchableOpacity } from "react-native";
+import { Stack } from "expo-router";
 
 export default function RootLayoutSettings() {
   return (
